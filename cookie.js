d620db01ca0fa6dfedae535af67ffce668533ef8
Deanna Vickers <deanna2000@gmail.com>
2018-04-10 14:31:45 -0500

    updated cookie file with comments

--- cookie.js
+++ cookie.js
@@ -1,8 +1,10 @@
 //C is for Cookie
+//Set up data and inialize variables
 const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 let y = 1
 
+//Iterate through the array
 for (let y = 0; y < cookies.length; y++) {
-    const currentCookie = cookies[y]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+    const currentCookie = cookies[y] //Set current array item to a variable
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`) //Use interpolation to write to the DOM, including the array item
 }
